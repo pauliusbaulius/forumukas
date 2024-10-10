e7b28fb245f43d0a74ea6e025c5c02671c23d503
Paulius Gervė <dev@paulius.xyz>
2024-10-10 21:36:12 +0300

    fix issues with email/username with django user model

--- core/models.py
+++ core/models.py
@@ -7,6 +7,7 @@ import nh3
 import pydantic
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -15,13 +16,53 @@ from core.utils import url_to_instance
 _logger = logging.getLogger("forumukas")
 
 
+class CustomUserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    def _create_user(self, email, password=None, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
 class CustomUser(AbstractUser):
-    # Blank custom user to allow for future customizations of the user
-    # without big pain.
-    # See: https://docs.djangoproject.com/en/5.1/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
     email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
+
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username or self.email
+
+    def save(self, *args, **kwargs):
+        if not self.username:
+            self.username = self.email  # Default username to email.
+        super().save(*args, **kwargs)
 
 
 
